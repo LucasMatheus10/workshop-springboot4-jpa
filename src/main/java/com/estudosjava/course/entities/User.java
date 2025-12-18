5fd9f79383990707cb9c80dcce3800ef979302f9
Suporte <suporte@iMac-de-EXAME.local>
2025-12-18 12:58:02 -0300

    H2 database, test profile e JPA adicionados

--- src/main/java/com/estudosjava/course/entities/User.java
+++ src/main/java/com/estudosjava/course/entities/User.java
@@ -1,20 +1,28 @@
 package com.estudosjava.course.entities;
 import java.io.Serializable;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 @Getter
 @Setter
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements java.io.Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
