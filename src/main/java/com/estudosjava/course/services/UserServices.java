28a9976a908e7e67fa270744eb96d8d8f27ab11e
Lucas Matheus <lucasmatheus24179@gmail.com>
2025-12-22 23:21:01 -0300

    Camada service e component registration

---
+++ src/main/java/com/estudosjava/course/services/UserServices.java
@@ -0,0 +1,23 @@
+package com.estudosjava.course.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudosjava.course.entities.User;
+import com.estudosjava.course.repositories.UserRepository;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
