5a2188711f111f0b4fae875a492bfa74fd059eac
Lucas Matheus <lucasmatheus24179@gmail.com>
2025-12-23 00:03:48 -0300

    Order Entity adicionada e ajustes na User Entity

--- src/main/java/com/estudosjava/course/entities/User.java
+++ src/main/java/com/estudosjava/course/entities/User.java
@@ -1,9 +1,13 @@
 package com.estudosjava.course.entities;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -29,4 +33,18 @@ public class User implements java.io.Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 }
