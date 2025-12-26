e12e7e0ac271da91c13a8b0b8e39046abed1c42f
Suporte <suporte@iMac-de-EXAME.local>
2025-12-26 14:03:29 -0300

    OrderItem Entity adicionada

---
+++ src/main/java/com/estudosjava/course/entities/OrderItem.java
@@ -0,0 +1,51 @@
+package com.estudosjava.course.entities;
+
+import com.estudosjava.course.entities.pk.OrderItemPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "tb_order_item")
+public class OrderItem implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+}
---
+++ src/main/java/com/estudosjava/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,28 @@
+package com.estudosjava.course.entities.pk;
+
+import com.estudosjava.course.entities.Order;
+import com.estudosjava.course.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Setter
+@Getter
+@EqualsAndHashCode
+public class OrderItemPK implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    
+}
