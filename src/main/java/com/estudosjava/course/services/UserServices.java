529760837b4e4dbc92c30b11480fa90fde2caa57
Suporte <suporte@iMac-de-EXAME.local>
2026-01-02 10:24:27 -0300

    Tratamento de exceções do método FindById

--- src/main/java/com/estudosjava/course/services/UserServices.java
+++ src/main/java/com/estudosjava/course/services/UserServices.java
@@ -1,11 +1,12 @@
 package com.estudosjava.course.services;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.estudosjava.course.entities.User;
 import com.estudosjava.course.repositories.UserRepository;
+import com.estudosjava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserServices {
@@ -18,7 +19,8 @@ public class UserServices {
     }
 
     public User findById(Long id) {
-        return repository.findById(id).get();
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
