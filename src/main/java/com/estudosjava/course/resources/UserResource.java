807b48eba0dacf38b97b894834f741081f854f01
Suporte <suporte@iMac-de-EXAME.local>
2026-01-02 09:55:07 -0300

    Atualização de usuário

--- src/main/java/com/estudosjava/course/resources/UserResource.java
+++ src/main/java/com/estudosjava/course/resources/UserResource.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -50,6 +52,12 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
     
 }
