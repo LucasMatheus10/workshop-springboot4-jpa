7f720057791b754cdabed9045953fe6994d229a9
Suporte <suporte@iMac-de-EXAME.local>
2026-01-02 11:38:06 -0300

    Ajustes nas demais entidades

--- src/main/java/com/estudosjava/course/services/OrderServices.java
+++ src/main/java/com/estudosjava/course/services/OrderServices.java
@@ -1,11 +1,14 @@
 package com.estudosjava.course.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudosjava.course.entities.Order;
 import com.estudosjava.course.repositories.OrderRepository;
+import com.estudosjava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderServices {
@@ -18,6 +21,7 @@ public class OrderServices {
     }
 
     public Order findById(Long id) {
-        return repository.findById(id).get();
+        Optional<Order> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 }
--- src/main/java/com/estudosjava/course/services/ProductService.java
+++ src/main/java/com/estudosjava/course/services/ProductService.java
@@ -1,11 +1,14 @@
 package com.estudosjava.course.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.estudosjava.course.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudosjava.course.repositories.ProductRepository;
+import com.estudosjava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -18,7 +21,8 @@ public class ProductService {
     }
 
     public Product findById(Long id) {
-        return repository.findById(id).get();
+        Optional<Product> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
 }
