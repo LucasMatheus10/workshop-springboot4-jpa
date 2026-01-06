e8bcadbe138d8623a344dc5a0a4c7486aaf3c31b
Lucas Matheus <lucasmatheus24179@gmail.com>
2026-01-05 22:56:01 -0300

    feat: Início do CRUD de Product, implementado o ProductService

--- src/main/java/com/estudosjava/course/services/ProductService.java
+++ src/main/java/com/estudosjava/course/services/ProductService.java
@@ -1,28 +1,81 @@
 package com.estudosjava.course.services;
 
 import java.util.List;
-import java.util.Optional;
-
+import com.estudosjava.course.dto.ProductDTO;
 import com.estudosjava.course.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.estudosjava.course.repositories.CategoryRepository;
 import com.estudosjava.course.repositories.ProductRepository;
+import com.estudosjava.course.services.exceptions.DatabaseException;
 import com.estudosjava.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
-    public List<Product> findAll() {
-        return repository.findAll();
+    public List<ProductDTO> findAll() {
+        List<Product> list = repository.findAll();
+        return list.stream().map(ProductDTO::new).toList();
     }
 
-    public Product findById(Long id) {
-        Optional<Product> obj = repository.findById(id);
-        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Product product = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+        return new ProductDTO(product);
     }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product product = new Product();
+        loadData(product, dto);
+        product = repository.save(product);
+        return new ProductDTO(product);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+            repository.flush();
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Registro possui dependências");
+        }
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        try {
+            Product product = repository.getReferenceById(id);
+            loadData(product, dto);
+            product = repository.save(product);
+            return new ProductDTO(product);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    public void loadData(Product product, ProductDTO dto) {
+        product.setName(dto.name());
+        product.setDescription(dto.description());
+        product.setPrice(dto.price());
+        product.setImgUrl(dto.imgUrl());
+        product.getCategories().clear();
+        if (dto.categories() != null) {
+            dto.categories().forEach(catDto -> {
+                product.getCategories().add(categoryRepository.getReferenceById(catDto.id()));
+            });
+        }
+    }
 }
