ceae11eba9171cc50ff71132209fec78f4cdc01d
LucasMatheus10 <lucasmatheus24179@gmail.com>
2026-01-08 10:24:10 -0300

    Order Resource implementado e adição do método cancel

--- src/main/java/com/estudosjava/course/resources/OrderResource.java
+++ src/main/java/com/estudosjava/course/resources/OrderResource.java
@@ -1,14 +1,24 @@
 package com.estudosjava.course.resources;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudosjava.course.dto.OrderDTO;
+import com.estudosjava.course.dto.OrderInsertDTO;
+import com.estudosjava.course.dto.OrderStatusDTO;
 import com.estudosjava.course.services.OrderServices;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -29,6 +39,26 @@ public class OrderResource {
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         OrderDTO obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderInsertDTO dto) {
+        OrderDTO newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newDto.id()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<OrderDTO> update(@PathVariable Long id, @Valid @RequestBody OrderStatusDTO dto) {
+        OrderDTO newDto = service.update(id, dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @PutMapping(value = "/{id}/cancel")
+    public ResponseEntity<OrderDTO> cancel(@PathVariable Long id) {
+        OrderDTO dto = service.cancel(id);
+        return ResponseEntity.ok().body(dto);
     }
     
 }
