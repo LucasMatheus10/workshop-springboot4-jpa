8a4b41d2e58073f3fe835c1543e30a9127cf7378
LucasMatheus10 <lucasmatheus24179@gmail.com>
2026-01-09 12:04:00 -0300

    feat: implementa autenticação JWT e segurança com Spring Security

---
+++ src/main/java/com/estudosjava/course/resources/AuthenticationResource.java
@@ -0,0 +1,36 @@
+package com.estudosjava.course.resources;
+
+import com.estudosjava.course.dto.AuthenticationDTO;
+import com.estudosjava.course.dto.LoginResponseDTO;
+import com.estudosjava.course.entities.User;
+import com.estudosjava.course.services.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+}
