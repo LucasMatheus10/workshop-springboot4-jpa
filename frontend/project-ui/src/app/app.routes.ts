abbf808aea3638ddeb2aeb188d73341c58feb662
Suporte <suporte@iMac-de-EXAME.local>
2026-01-15 12:55:44 -0300

    Telas de login e registro iniciadas

--- frontend/project-ui/src/app/app.routes.ts
+++ frontend/project-ui/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  // Tela de Login
+  { 
+    path: 'login', 
+    loadComponent: () => import('./components/login/login').then(m => m.Login) 
+  },
+  
+  // Tela de Registro (Adicionei aqui para você)
+  { 
+    path: 'register', 
+    loadComponent: () => import('./components/register/register').then(m => m.Register) 
+  },
+  
+  // Rotas de Usuário Comum
+  { 
+    path: 'products', 
+    loadComponent: () => import('./components/product-list/product-list').then(m => m.ProductList) 
+  },
+  { 
+    path: 'payment', 
+    loadComponent: () => import('./components/payment/payment').then(m => m.Payment) 
+  },
+  
+  // Rotas de Administrador
+  { 
+    path: 'admin', 
+    loadComponent: () => import('./components/admin/dashboard/dashboard').then(m => m.Dashboard) 
+  },
+  { 
+    path: 'admin/categories', 
+    loadComponent: () => import('./components/admin/category-manager/category-manager').then(m => m.CategoryManager) 
+  }
+];
---
+++ frontend/project-ui/src/app/components/register/register.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+  userData = {
+    name: '',
+    email: '',
+    phone: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  onRegister() {
+    console.log('Enviando para o backend:', this.userData);
+    // Integração futura com o AuthService (POST /users)
+    // Após o sucesso:
+    alert('Usuário cadastrado com sucesso!');
+    this.router.navigate(['/login']);
+  }
+}
