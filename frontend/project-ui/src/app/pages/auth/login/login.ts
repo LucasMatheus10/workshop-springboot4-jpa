cc60cf8f0b759261a8a50983e1e8e2fb0570047e
LucasMatheus10 <lucasmatheus24179@gmail.com>
2026-01-22 13:00:11 -0300

    Adição da barra de busca e ajustes no layout

--- frontend/project-ui/src/app/pages/auth/login/login.ts
+++ frontend/project-ui/src/app/pages/auth/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,7 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './login.css'
 })
 export class LoginComponent {
+  private authService = inject(AuthService);
   loginData = { email: '', password: '' };
 
   constructor(private router: Router) {}
@@ -18,7 +21,7 @@ export class LoginComponent {
     console.log('Tentando logar com:', this.loginData);
     // Aqui chamaremos o backend
     if (this.loginData.email && this.loginData.password) {
-      this.router.navigate(['/products']); // Vai para a SPA de produtos
+      this.authService.login('user-token-fake'); // Simula um token
     }
   }
 }
--- frontend/project-ui/src/app/services/product.service.ts
+++ frontend/project-ui/src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Product } from '../models/product.model';
 
 @Injectable({ providedIn: 'root' })
@@ -8,7 +8,17 @@ export class ProductService {
   private http = inject(HttpClient);
   private readonly API = 'http://localhost:8080/products';
 
+  // Guarda o termo de busca atual
+  private searchTermSubject = new BehaviorSubject<string>('');
+  
+  searchTerm$ = this.searchTermSubject.asObservable();
+
   findAll(): Observable<Product[]> {
     return this.http.get<Product[]>(this.API);
+  }
+
+  // Atualiza o termo (chamado pela Navbar)
+  updateSearchTerm(term: string): void {
+    this.searchTermSubject.next(term);
   }
 }
