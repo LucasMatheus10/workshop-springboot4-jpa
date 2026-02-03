33e9a8c43872574d7434cd3944840ab1ebc6c9ce
LucasMatheus10 <lucasmatheus24179@gmail.com>
2026-02-03 12:15:41 -0300

    fix: Ajuste nas inserções do data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,19 +2,19 @@
 -- Data for Name: tb_user; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO tb_user (id, email, name, password, phone, role) VALUES
-(1, 'john.doe@email.com', 'John Doe', '$2a$10$QsOFlxuIlaK7wL2Jgh3zKOvIvdoTrdn23eNl5G02Cbp5YZT7.fmXG', '1234567890', 'ADMIN'),
-(2, 'jane.smith@email.com', 'Jane Smith', '$2a$10$sA63g7KW00eIFAK6o9y6Ruvl/L8azPoL5ypkw5OQQktd5x0eSjH26', '0987654321', 'USER');
+INSERT INTO tb_user (email, name, password, phone, role) VALUES
+('john.doe@email.com', 'John Doe', '$2a$10$QsOFlxuIlaK7wL2Jgh3zKOvIvdoTrdn23eNl5G02Cbp5YZT7.fmXG', '1234567890', 'ADMIN'),
+('jane.smith@email.com', 'Jane Smith', '$2a$10$sA63g7KW00eIFAK6o9y6Ruvl/L8azPoL5ypkw5OQQktd5x0eSjH26', '0987654321', 'USER');
 
 
 --
 -- Data for Name: tb_order; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO tb_order (id, moment, order_status, client_id) VALUES
-(2, '2024-06-02 08:30:00-03', 'WAITING_PAYMENT', 2),
-(3, '2024-06-03 11:15:00-03', 'SHIPPED', 1),
-(1, '2026-01-28 14:15:10.128047-03', 'PAID', 1);
+INSERT INTO tb_order (moment, order_status, client_id) VALUES
+('2024-06-02 08:30:00-03', 'WAITING_PAYMENT', 2),
+('2024-06-03 11:15:00-03', 'SHIPPED', 1),
+('2026-01-28 14:15:10.128047-03', 'PAID', 1);
 
 --
 -- Data for Name: payment; Type: TABLE DATA; Schema: public; Owner: -
@@ -28,26 +28,26 @@ INSERT INTO payment (moment, order_id) VALUES
 -- Data for Name: tb_category; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO tb_category (id, name) VALUES
-(1, 'Electronics'),
-(2, 'Books'),
-(3, 'Clothing'),
-(52, 'Periféricos'),
-(53, 'Monitores');
+INSERT INTO tb_category (name) VALUES
+('Electronics'),
+('Books'),
+('Clothing'),
+('Periféricos'),
+('Monitores');
 --
 -- Data for Name: tb_product; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO tb_product (id, description, img_url, name, price) VALUES
-(2, '42 inch smart TV', 'https://www.hisense.com.br/_next/image?url=%2FA4N%2F1.png&w=2048&q=100', 'Smart TV', 2190),
-(3, 'Apple laptop', 'https://t4.ftcdn.net/jpg/06/01/14/23/360_F_601142328_VnY6DMf1sC0RULodemaCSrvXSlFhO1lA.jpg', 'Macbook Pro', 6250),
-(4, 'High performance gaming PC', 'https://images.unsplash.com/photo-1587202372775-e229f172b9d7?auto=format&fit=crop&q=80&w=1000', 'PC Gamer', 3200),
-(5, 'A beginner''s guide to Rails', 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?auto=format&fit=crop&q=80&w=1000', 'Rails for Dummies', 100.99),
-(6, 'Noise-cancelling headphones', 'https://www.pngall.com/wp-content/uploads/5/Headset-PNG-Image-HD.png', 'Headset', 299.99),
-(7, 'Camiseta algodão 100%', 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1000', 'Camiseta Tech', 59.9),
-(8, 'Moletom com capuz confortável', 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=1000', 'Moletom Gamer', 150),
-(9, 'Boné aba curva preto', 'https://images.unsplash.com/photo-1588850561407-ed78c282e89b?q=80&w=1000', 'Boné Estilizado', 45),
-(1, 'A fantasy novel', 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?auto=format&fit=crop&q=80&w=1000', 'The Lord of the Rings', 90.99);
+INSERT INTO tb_product (description, img_url, name, price) VALUES
+('42 inch smart TV', 'https://www.hisense.com.br/_next/image?url=%2FA4N%2F1.png&w=2048&q=100', 'Smart TV', 2190),
+('Apple laptop', 'https://t4.ftcdn.net/jpg/06/01/14/23/360_F_601142328_VnY6DMf1sC0RULodemaCSrvXSlFhO1lA.jpg', 'Macbook Pro', 6250),
+('High performance gaming PC', 'https://images.unsplash.com/photo-1587202372775-e229f172b9d7?auto=format&fit=crop&q=80&w=1000', 'PC Gamer', 3200),
+('A beginner''s guide to Rails', 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?auto=format&fit=crop&q=80&w=1000', 'Rails for Dummies', 100.99),
+('Noise-cancelling headphones', 'https://www.pngall.com/wp-content/uploads/5/Headset-PNG-Image-HD.png', 'Headset', 299.99),
+('Camiseta algodão 100%', 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=1000', 'Camiseta Tech', 59.9),
+('Moletom com capuz confortável', 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?q=80&w=1000', 'Moletom Gamer', 150),
+('Boné aba curva preto', 'https://images.unsplash.com/photo-1588850561407-ed78c282e89b?q=80&w=1000', 'Boné Estilizado', 45),
+('A fantasy novel', 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?auto=format&fit=crop&q=80&w=1000', 'The Lord of the Rings', 90.99);
 
 
 --
@@ -73,36 +73,8 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES
 (7, 3),
 (8, 3),
 (9, 3),
-(6, 52),
-(2, 53);
-
---
--- Name: tb_category_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT setval('tb_category_seq', 101, true);
-
-
---
--- Name: tb_order_seq; Type: SEQUENCE SET; Schema: public; Owner: -
-
-
-SELECT setval('tb_order_seq', 101, true);
-
-
---
--- Name: tb_product_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT setval('tb_product_seq', 51, true);
-
-
---
--- Name: tb_user_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT setval('tb_user_seq', 101, true);
-
+(6, 3),
+(2, 5);
 
 --
 -- PostgreSQL database dump complete
