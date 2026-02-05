f7d826b6925586a028661a7eff02ce05f09c1b23
LucasMatheus10 <lucasmatheus24179@gmail.com>
2026-02-05 09:50:23 -0300

    Ajuste nas categorias

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -42,16 +42,16 @@ INSERT INTO tb_order_item (order_id, product_id, quantity, price) VALUES
 
 -- TB_PRODUCT_CATEGORY
 INSERT INTO tb_product_category (product_id, category_id) VALUES
-(1, 2),
+(1, 5),
+(1, 1),
 (2, 1),
 (3, 1),
-(4, 1),
-(5, 2),
+(4, 2),
+(5, 4),
+(6, 3),
 (7, 3),
 (8, 3),
-(9, 3),
-(6, 3),
-(2, 5);
+(9, 2),
 
 
 ALTER SEQUENCE tb_user_seq RESTART WITH 3;
